3147cdb6acb978bef62031c4cea59f39a955f503
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-25 00:16:01 +0700

    show add edit delete

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>Name List</h1>
-    username :<input type ="text" v-model="username">
-    email :<input type ="email" v-model="email">
+    username : <input type="text" v-model="username" /> email :
+    <input type="email" v-model="email" />
     <button @click="doAdd">Add Customer</button>
     <table>
       <thead>
@@ -14,11 +14,13 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="c in customers" :key="c.id">
+        <tr v-for="(c, index) in customers" :key="c.id">
           <td>{{ c.id }}</td>
           <td>{{ c.username }}</td>
           <td>{{ c.email }}</td>
-          <td><button @click="doEdit">Edit</button><button v-on:click="doDelete(c.id)">Delete</button></td>
+          <td>
+            <button @click="doEdit">Edit</button><button @click="doDelete(index)">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -28,8 +30,8 @@
 export default {
   data() {
     return {
-        username:'',
-        email:'',
+      username: "",
+      email: "",
       customers: [
         { id: 1, username: "sunvodz1", email: "sunvo1@sunvo.sunvo" },
         { id: 2, username: "sunvodz2", email: "sunvo2@sunvo.sunvo" },
@@ -39,19 +41,21 @@ export default {
   },
   methods: {
     doAdd() {
-        let id = this.customers.length + 1
+      let id = this.customers[this.customers.length - 1].id + 1;
       this.customers.push({
         id: id,
         username: this.username,
-        email: this.email+"@sunvo.sunvo"
+        email: this.email + "@sunvo.sunvo"
       });
     },
     doEdit() {
-        console.log("Edit");
+      console.log("Edit");
     },
     doDelete(id) {
-        console.log("Delete"+id);
-    }
+      console.log("Delete" + id);
+      this.$delete(this.customers, id)
+    },
+
   }
 };
 </script>
